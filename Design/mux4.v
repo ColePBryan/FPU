a5b357dc27685dcd0d6128dedf27c7537ccee9e8
bryan <bryan.cole@sjsu.edu>
2018-10-09 19:38:16 -0700

    Switched to small calculator example

--- Design/mux4.v
+++ Design/mux4.v
@@ -1,6 +1,4 @@
-module mux4 # (
-        parameter WIDTH = 32
-    ) (
+module mux4 #(parameter WIDTH = 4) (
         input  wire [1:0]       sel,
         input  wire [WIDTH-1:0] in0,
         input  wire [WIDTH-1:0] in1,
@@ -17,5 +15,5 @@ module mux4 # (
             2'b11: out = in3;
         endcase
     end
-
+    
 endmodule
